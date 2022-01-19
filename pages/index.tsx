003cd9cc047f71fbe1ad8311ce14e67af3f9c7b9
Michael Howell <michael@nunc.co.nz>
2022-01-20 00:04:55 +1300

    Setup app framework

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Welcome!</title>
+      </Head>
+      <p>Hello world!</p>
+    </div>
+  )
+}
+
+export default Home
